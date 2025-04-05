c1c3b66c5088e0f9b4a6b79fc1cce7c33b019de7
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-05 11:24:35 +0900

    UserInput 컴포넌트 UI 구성

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
 import { type FormEvent } from "react";
 
 export const UserInput = () => {
@@ -7,5 +11,19 @@ export const UserInput = () => {
     e.preventDefault();
   };
 
-  return <form onSubmit={handleFormOnSubmit}></form>;
+  return (
+    <form
+      onSubmit={handleFormOnSubmit}
+      className="absolute left-[25%] top-8 w-1/2 space-y-4"
+    >
+      <Textarea placeholder="내용 입력~~~" />
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <Input placeholder="닉내임" />
+          <Input placeholder="비번" />
+        </div>
+        <Button>입력</Button>
+      </div>
+    </form>
+  );
 };
