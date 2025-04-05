d35044c9526db8bae799a26805e6cd5cc78f0a3c
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-05 11:16:50 +0900

    Metadata 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,11 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: ""
+  title: "Sibal",
+  description: "아무 말이나 하는 곳",
+  icons: {
+    icon: "/icons/lion.svg"
+  }
 };
 
 export default function RootLayout({
